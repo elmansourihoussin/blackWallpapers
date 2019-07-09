322d613d0ed09e99e9a7aed2d733aaf8100ea2d9
houssin mansouri <houssin.mansouri93@gmail.com>
2019-07-09 14:56:16 +0000

    ads

--- black/WallpapersHd/app/src/main/java/com/islamiat/blackwallpapers/admob/AdConfig.java
+++ black/WallpapersHd/app/src/main/java/com/islamiat/blackwallpapers/admob/AdConfig.java
@@ -13,16 +13,16 @@ public class AdConfig {
 
 
     // Admob app id
-    // Sample AdMob app ID: ca-app-pub-6142871159558873~5791444278
-    public static final String ADMOB_APP_ID = "ca-app-pub-6142871159558873~5791444278";
+    // Sample AdMob app ID: ca-app-pub-6142871159558873~3459513143
+    public static final String ADMOB_APP_ID = "ca-app-pub-6142871159558873~3459513143";
 
     // admob Banner Ads id
-    // Sample AdMob Banner Ads ID: ca-app-pub-6142871159558873/8772830839
-    public static final String BANNER_AD_UNIT_ID = "ca-app-pub-6142871159558873/8772830839";
+    // Sample AdMob Banner Ads ID: ca-app-pub-6142871159558873/5786718615
+    public static final String BANNER_AD_UNIT_ID = "ca-app-pub-6142871159558873/5786718615";
 
 
     // admob Interstitial Ads id
-    // Sample AdMob Interstitial Ads ID: ca-app-pub-6142871159558873/1991800065
-    public static final String INTERSTITIAL_AD_UNIT_ID = "ca-app-pub-6142871159558873/1991800065";
+    // Sample AdMob Interstitial Ads ID: ca-app-pub-6142871159558873/9151248551
+    public static final String INTERSTITIAL_AD_UNIT_ID = "ca-app-pub-6142871159558873/9151248551";
 
 }
